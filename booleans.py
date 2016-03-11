c123bce75ebefcb120c1ef1e0fba2c5c7633481f
mckennasheridan <sheri111@mail.chapman.edu>
2016-03-11 02:38:28 -0800

    partially finished booleans

---
+++ booleans.py
@@ -0,0 +1,21 @@
+C = 41
+#Sets variable C equal to 41
+C == 40
+#Sets variable C equal to 40
+C!=40 and C<40
+#C cannot equal 40 AND C must be less than 40 to make statement True
+C!=40 or C<41
+#Only one of the two conditions needs to be true to make statement True
+not C == 40
+#If the C equals 40, the statment is false. This basically reverses the evaluation of C == 40.
+not C>40
+#If C is greater than 40, statement is false.
+C <= 41
+#C must be less than or equal to 41 in order for the statement to be True.
+not False
+True and False
+False or False or False
+True and True and False
+False == 0
+True == 0
+True == 1
